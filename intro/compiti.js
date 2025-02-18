af903079591a3c93d2193bca2b7e0ea0303d8bd0
halex22 <halex2209@gmail.com>
2025-02-18 22:57:28 +0100

    up to exercise 8

--- intro/compiti.js
+++ intro/compiti.js
@@ -52,8 +52,53 @@ console.log(resultString)
 // for (let i = 100; i < -101; i--) {
 //   console.log(i)
 // }
-// 4 exercise 
 
-for (let i = 100; i >= -100; i--) {
-  console.log(i)
+// 4 exercise 
+// console.log('4 exercise')
+// for (let i = 100; i >= -100; i--) {
+//   console.log(i)
+// }
+
+// 5 exercise 
+// console.log('5 exercise')
+// for (let index = 0; index <= 100; index++) {
+//   // if (!(index % 7)) {
+//   //   console.log(`${index} *`)
+//   // } else {
+//   //   console.log(index)
+//   // }
+//   const outputToPrint = index % 7 ? index : `${index} *`
+//   console.log(outputToPrint)
+// }
+
+// 6 exercise 
+// console.log('6 exercise')
+// for (let i = 0; i <= 100; i++) {
+//   const result = Math.sqrt(i)
+//   if (result % 1 !== 0) {
+//     console.log(result)
+//   } else {
+//     console.log(`${result} *`)
+//   }
+// }
+
+// 7 
+// let userNumber
+// while (true) {
+//   userNumber = parseInt(prompt('please provide a positive number...'))
+//   if (userNumber >= 0 ) break
+// }
+// for (let i = userNumber; i >= 0; i--) {
+//   console.log(i)
+// }
+
+// 8 exercise 
+const userNumber = parseInt(prompt('Please provide a number', 5))
+
+let index = userNumber
+const updateFactor = userNumber < 100 ? 1 : -1
+while (index !== 100) {
+  console.log(index)
+  index += updateFactor
 }
+console.log(100)

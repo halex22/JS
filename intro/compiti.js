46c402833c7a4eac03e19eb042003b9e203fb958
halex22 <halex2209@gmail.com>
2025-02-18 19:39:41 +0100

    update up to 5th exercise

--- intro/compiti.js
+++ intro/compiti.js
@@ -1,6 +1,6 @@
-// 1 esercizio 
+// 1 exercise 
 let resultString = ''  
-
+console.log('1 exercise')
 for (let i = 0; i < 7; i++) {
   
   for (let j = 0; j <= i; j++) {
@@ -11,14 +11,49 @@ for (let i = 0; i < 7; i++) {
 console.log(resultString)
 
 
-// 2 esercizio 
- resultString = ''  
-
-for (let i = 0; i < 7; i++) {
-  
-  for (let j = 0; j <= i; j++) {
+// 2 exercise 
+resultString = ''  
+console.log('2 exercise')
+for (let row = 7; row > 0 ; row--) {
+  for (let j = 0; j < row; j++) {
     resultString += '#'
   }
-  resultString += "\n" 
+  resultString += "\n"
 }
 console.log(resultString)
+
+
+// 3 exercise
+resultString = ''  
+console.log('3 exercise')
+let hashTagNumber = 1
+
+for (let index = 0; index < 5; index++) {
+
+  const numberOfBlankSpaces = (9 - hashTagNumber) / 2
+  innerRow = ''
+  while (innerRow.length < 9) {
+    for (let blackSpace = 0; blackSpace < numberOfBlankSpaces; blackSpace++) {
+      innerRow += ' '
+    }
+    if (innerRow.length >= 9) break
+    for (let hastTag = 0; hastTag < hashTagNumber; hastTag++) {
+      innerRow += '#'
+    }
+  }
+  resultString += `${innerRow}\n`
+  hashTagNumber += 2
+
+}
+console.log(resultString)
+
+
+
+// for (let i = 100; i < -101; i--) {
+//   console.log(i)
+// }
+// 4 exercise 
+
+for (let i = 100; i >= -100; i--) {
+  console.log(i)
+}

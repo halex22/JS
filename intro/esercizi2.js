e6d2422e906940e2a3ad64b1f449e7bb4f12d5fc
halex22 <halex2209@gmail.com>
2025-02-19 22:22:48 +0100

    exercise 2 complete

--- intro/esercizi2.js
+++ intro/esercizi2.js
@@ -11,22 +11,38 @@ console.log(minArrow(5,4))
 // even or odd 
 // zero is even - one is odd
 function isEven(number) {
-  if ((number - 2) === 1) {
-    return false
-  } else if ((number - 2) === 0) {
-    return true
-  }
+  if (number >= 2 ) return isEven((number - 2 ))
+  return number === 0 ? true : false
 }
 
+const isEvenArrow = number => {
+  if (number >= 2) return isEvenArrow((number - 2))
+    return number ? false : true
+}
+
+console.log(isEven(72))
+console.log('is 14 even? ',isEvenArrow(14))
+console.log('is 21 even? ',isEvenArrow(21))
+
+
 //  char counter
-// function countBs(stringToCheck) {
-//   let numberOfBs = 0
+function countBs(stringToCheck) {
+  let count = 0
+  for (let charIndex = 0; charIndex < stringToCheck.length; charIndex++) {
+    if (stringToCheck[charIndex] === 'B') count++
+  }
+  return count
+}
 
-//   for (let letter = 0; letter < ; letter++) {
-    
-//   }
+console.log(countBs('Bouna Notte Belli'))
 
-// }
 
-const test = 'qusta è una stringa'
-console.log(test[3])
+const countChar = (stringToCheck, characterToCheck)  => {
+  let count = 0
+  for (let charIndex = 0; charIndex < stringToCheck.length; charIndex++) {
+      if (stringToCheck[charIndex] === characterToCheck) count ++    
+  }
+  return count
+}
+
+console.log(countChar('this is the string', 'w'))

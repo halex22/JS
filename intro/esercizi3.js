5e5842e07480b247ab7b01d419fb8ca85929f90b
halex22 <halex2209@gmail.com>
2025-02-20 23:10:50 +0100

    up to exercise 14

--- intro/esercizi3.js
+++ intro/esercizi3.js
@@ -12,7 +12,7 @@ return filteredArray
 console.log(removeGraterThan30(array1))
 
 // 2nd exercise
-// converto elements to fahrenheit
+// convert elements to fahrenheit
 const celToFah = number => (number * (9/5) + 32)
 const convertDegrees = arrayToMap => {
   const mappedArray = []
@@ -24,7 +24,7 @@ const convertDegrees = arrayToMap => {
 console.log(convertDegrees(array1))
 
 // 3rd exercise 
-// calcolate the mean
+// calculate the mean
 const calculateMean = arrayToReduce => {
   let accumulator = 0
   for (const number of arrayToReduce) {
@@ -52,7 +52,7 @@ const findMaxAndMin = arrayToReduce => {
         if (number < minNumber) minNumber=number
       }
     }
-    lastNumber = number
+  lastNumber = number
   }
   return [maxNumber, minNumber]
 }
@@ -66,41 +66,129 @@ const array2 = ['pippo', 'pluto', 'paperino', 'qui', 'quo', 'qua', 'clarabella',
 // 5th exercise
 // filter name.length < 4
 const filterNames = arrayToFilter => {
-  
+  const filteredArray = []
+  for (const name of arrayToFilter) {
+    if (name.length < 4) filteredArray.push(name)
+  }
+  return filteredArray
 }
+console.log(filterNames(array2))
 
 // 6th exercise 
 // transform names to uppercase
+const mapToUpperCase = arrayToMap => {
+  const mappedArray = []
+  for (const name of arrayToMap) {
+    mappedArray.push(name.toUpperCase())    
+  }
+  return mappedArray
+}
+console.log(mapToUpperCase(array2))
 
 // 7th exercise
 // capitalize each string
+const capitalizeString = text => {
+  let capitalizedText = ''
+  capitalizedText += text[0].toUpperCase()
+  capitalizedText += text.slice(1, text.length)
+  return capitalizedText
+}
 
+const capitalizeNames = arrayToMap => {
+  const mappedArray = []
+  for (const name of arrayToMap) {
+    mappedArray.push(capitalizeString(name))
+  }
+  return mappedArray
+}
+console.log(capitalizeNames(array2))
 
 // 8th exercise
 // return string with first char of all names
-
+const createStringInitial = arrayToReduce => {
+  let resultString = ''
+  for (const name of arrayToReduce) {
+    resultString += name[0]
+  }
+  return resultString
+}
+console.log(createStringInitial(array2))
 
 // 9th exercise 
 // count the sum of all char of all the names
-
+const countAllChars = arrayToReduce => {
+  let accumulator = 0
+  for (const name of arrayToReduce) {
+    accumulator += name.length
+  }
+  return accumulator
+}
+console.log(countAllChars(array2))
 
 // 10th exercise 
 // delete vowels from names
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const isVowel = char => vowels.includes(char)
 
+const deleteVowels = arrayToMap => {
+  const mappedArray = []
+  for (const name of arrayToMap) {
+    let mappedString = ''
+    for (const letter of name) {
+      if (!isVowel(letter)) mappedString += letter
+    }
+    mappedArray.push(mappedString)
+  }
+  return mappedArray
+}
+console.log(deleteVowels(array2))
 
 // 11th exercise 
 // return array with each name length 
+const countNamesLength = arrayToMap => {
+  const mappedArray = []
+  for (const name of arrayToMap) {
+    mappedArray.push(name.length)
+  }
+  return mappedArray
+}
+console.log(countNamesLength(array2))
 
 
+// section 3
 const array3 = [[1,2,3], [3,2,1,0], [0,0,0,0,0]]
 
 // 12th exercise 
 // return an array with the length of each array 
-
+const countArraysLengths = arrayToMap => {
+  const mappedArray = []
+  for (const singleArray of arrayToMap) {
+    mappedArray.push(singleArray.length)
+  }
+  return mappedArray
+}
+console.log(countArraysLengths(array3))
 
 // 13 exercise 
 // sum all number in the arrays
 
+const sumAllNumbers = arrayToReduce => {
+  let accumulator = 0
+  for (const number of arrayToReduce) {
+    accumulator += number
+  }
+  return accumulator
+}
+
+const sumMultipleArrays = arrayToReduce => {
+  let accumulator = 0
+  for (const singleArray of arrayToReduce) {
+    accumulator += sumAllNumbers(singleArray)
+  }
+  return accumulator
+}
+
+console.log(sumMultipleArrays(array3))
 
 // 14th exercise 
 // sum all arrays length 

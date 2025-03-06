ec4c6ac36fb5d01151426a7178f5261f37446945
halex22 <halex2209@gmail.com>
2025-03-06 13:24:13 +0100

    working on canvas

---
+++ projects/confetti/index.mjs
@@ -0,0 +1,12 @@
+import JSConfetti from './node_modules/js-confetti/dist/es/index.js'
+
+const jsConfetti = new JSConfetti()
+
+
+
+// function activateConfetti() {
+//   console.log('ciao')
+// }
+
+// console.log('ciao')
+jsConfetti.addConfetti()
